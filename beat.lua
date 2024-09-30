8e408deb1b8f1b1476f307a37d622251bb9b5d22
HoneycombMegaphone <178117678+HoneycombMegaphone@users.noreply.github.com>
2024-09-30 02:07:25 +0100

    fixed timings and music staff; added sample chart demo from sample beat

--- beat.lua
+++ beat.lua
@@ -2,7 +2,8 @@
 
 beat = {
 	power = true,
-	tempo = 30,
+	note = 30, --default to beat
+	deafult = 0,
 	sprite = 0,
 	count = 30,
 	reverb = 2,
@@ -11,7 +12,7 @@ beat = {
 	cooldown = 0,
 	button = 5,
 	sound = 0,
-
+	chart = {},
 	new = function(self, table)
 		table = table or {}
 		setmetatable(table, {
@@ -26,10 +27,10 @@ beat = {
 
 	update = function(self)
 		if (self.cooldown == 0) then
-			self.sprite = 0
+			self.sprite = self.default
 			if btnp(self.button) then
-				if self.count <= 3 
-					or self.count >= self.tempo - 3 then
+				if ceil(self.count) <= 3
+					or flr(self.count) >= self.note - 2 then
 					self.sprite = 2
 				else
 					self.sprite = 4
@@ -40,27 +41,39 @@ beat = {
 		else
 			self.cooldown = self.cooldown - 1
 		end
-		self.count = self.count - 1
 
-		if self.count == 1 then
+		if flr(self.count) == 2 then
 			self.y = self.y - 1
-		elseif self.count == 0 then
+		elseif flr(self.count) == 1 then
 			self.x = self.x - 1
 			self.y = self.y + 3
 			--sfx(beat.sound)
-		elseif self.count < 0 then
+		elseif flr(self.count) == 0 then
 			self.x = self.x + 1
 			self.reverb = 1 
-			self.count = self.tempo - 1
+			if self.chart == {} then
+				self.count = self.count + 30
+			else
+				self.note = self.chart[1]
+				deli(self.chart, 1)
+				self.count = self.count + self.note
+			end
 		end
 		if self.reverb >= 0 then
 			self.reverb = self.reverb - 1
 			self.y = self.y - 1
 		end
+		self.count = self.count - 1
+	end,
+	addRhythm = function(self, table)
+		table = table or {}
+		for notes in all(table) do
+			add(self.chart, notes)
+		end
 	end
 }
 
-beat1 = beat:new({x = 20, y = 20, button = 0})
-beat2 = beat:new({x = 20, y = 40, button = 2})
-beat3 = beat:new({x = 20, y = 60, button = 3})
-beat4 = beat:new({x = 20, y = 80, button = 1})
+beat1 = beat:new({default = 32, x = 20, y = 20, button = 0, chart = {}})
+beat2 = beat:new({default = 34, x = 20, y = 40, button = 2, chart = {}})
+beat3 = beat:new({default = 36, x = 20, y = 60, button = 3, chart = {}})
+beat4 = beat:new({default = 36, x = 20, y = 80, button = 1, chart = {}})
--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
 --main
 
+clock = {
+	tempo = 30, -- global beat
+	count = 30,
+	update = function(self)
+		if self.count == 0 then self.count = clock.tempo end
+		self.count = self.count - 1
+	end
+}
+
 staff = {
 	sprite = 6,
 	x = 128,
@@ -11,10 +20,8 @@ staff = {
 		return table
 	end,
 	update = function(self)
-		self.x = self.x - 32/30
-		if (self.x <= 0) then
-			self.x = 128 - self.x
-		end
+		if self.x == 0 then	self.x = 120 end
+		self.x = self.x - 1
 	end,
 	draw = function(self)
 		for y = 20 + noteDist, 80 + noteDist, 20 do
@@ -23,29 +30,31 @@ staff = {
 	end
 }
 
-noteDist = 4
-
 function _init()
-	cls()
+	cls(1)
+	noteDist = 4
+	ongoing = false
+	duration = 0 
+	playSample = false
 	beats = {beat1, beat2, beat3, beat4}
-	--sheet = {measure, staff2, staff3, staff4}
 	sheet = {}
 	for i = 0, 15 do
 		if (i / 4 < 1) then
-			add(sheet, staff:new({x = 116, sprite = 7}))
+			add(sheet, staff:new({x = 113, sprite = 7}))
 		elseif (i / 4 < 2) then
-			add(sheet, staff:new({x = 84, sprite = 7}))
+			add(sheet, staff:new({x = 83, sprite = 7}))
 		elseif (i / 4 < 3) then
-			add(sheet, staff:new({x = 52, sprite = 7}))
+			add(sheet, staff:new({x = 53, sprite = 7}))
 		else
-			add(sheet, staff:new({x = 20, sprite = 8}))
+			add(sheet, staff:new({x = 23, sprite = 8}))
 		end
 	end
-	music(0)
 end
 
 function _draw()
-	cls()
+	cls(1)
+	print("Use Arrow keys to tap to beat")
+	print("To Play Sample, hit Z")
 	for x = 0, 120, 8 do
 		for y = 20 + noteDist, 80 + noteDist, 20 do
 			spr(6, x, y)
@@ -56,15 +65,43 @@ function _draw()
 	end
 	for beat in all(beats) do
 		beat:draw()
-		--print('beat.count: ' ..beat.count)
 	end
 end
 
 function _update()
+	clock:update()
 	for beat in all(beats) do
 		beat:update()
 	end
 	for staff in all(sheet) do
 		staff:update()
 	end
+	if btnp(4) and ongoing == false then playSample = true end
+	if clock.count == 1 and ongoing == false and playSample == true then 
+		addSample() 
+	end
+	if duration > 0 and clock.count == 1 then
+		duration -= 1
+		if duration == 0 then 
+			for beat in all(beats) do
+				beat.count = clock.count
+			end
+			ongoing = false 
+		end
+	end
+end
+
+function addSample()
+	beat1:addRhythm({30, 15, 15, 30, 30})
+	beat1:addRhythm({30, 30, 30, 30})
+	beat2:addRhythm({15, 15, 15, 15, 30, 30})
+	beat2:addRhythm({30, 15, 15, 15, 30, 15})
+	beat3:addRhythm({15, 15, 15, 15, 15, 15, 15, 15})
+	beat3:addRhythm({15, 15, 15, 15, 15, 15, 15, 15})
+	beat4:addRhythm({7.5, 15, 15, 15, 15, 15, 15, 15})
+	beat4:addRhythm({15, 15, 15, 15, 15, 15, 15})
+	music(0)
+	ongoing = true
+	playSample = false
+	duration = 9
 end
